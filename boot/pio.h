cb56f42e086a1b3fee9e96449c0341152137b298
Hojun-Cho <ghwns6743@gmail.com>
2024-12-02 10:19:57 +0900

    add trap
    don't want handle trap now

--- boot/pio.h
+++ boot/pio.h
@@ -1,8 +1,14 @@
-#define outb(port, data) \
-	(__outb(port, data)) 
-
 static __inline void
-__outb(int port, unsigned char data)
+outb(int port, u8 data)
 {
 	__asm volatile("outb %0,%w1" : : "a" (data), "d" (port));	
 }
+
+static __inline u8
+inb(int port)
+{
+	u8	data;
+
+	__asm volatile("inb %w1,%0" : "=a" (data) : "d" (port));
+	return data;
+};
